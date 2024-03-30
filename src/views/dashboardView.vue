edf94ed98f54be20a39f2d5bcd218f5b63ba1cbc
mjtampus <147782263+mjtampus@users.noreply.github.com>
2024-03-31 05:21:35 +0800

    Update dashboardView.vue

--- src/views/dashboardView.vue
+++ src/views/dashboardView.vue
@@ -39,9 +39,9 @@
 import { ref, computed } from 'vue';
 
 const images = [
-  'src/assets/image5.jpg',
-  'src/assets/image2.jpg',
-  'src/assets/image4.jpg'
+  '/src/assets/image5.jpg',
+  '/src/assets/image2.jpg',
+  '/src/assets/image4.jpg'
 ];
 
 const currentIndex = ref(0);
